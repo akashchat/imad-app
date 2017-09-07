3dd449e2292f6ec499a05676dc58140767f4ad06
akash <31346414+akashchat@users.noreply.github.com>
2017-09-07 18:08:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleName=req.params.articleName
+
 app.get('/:articleName', function (req, res) {
   res.send(createtemplate(articles[articleName]));
 });

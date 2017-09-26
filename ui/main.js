704d8394ace06652f372e8d6031871071c56ad5a
akash <31346414+akashchat@users.noreply.github.com>
2017-09-27 00:30:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var element =document.getElementById("elem");
+element.innerHTML="new text";
+var move= document.getElementById("dino");
+var marginleft=0;
+function moveRight(){
+    marginleft=marginleft+'10';
+    dino.style.marginleft= margin_left +'px';
+}
+dino.onclick= function (){
+    var interval=setInterval(moveRight,50);
+};

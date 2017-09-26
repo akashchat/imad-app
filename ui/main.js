f06376178b742ec7b2020cf988b6e1a31817a3f1
akash <31346414+akashchat@users.noreply.github.com>
2017-09-27 00:42:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,12 @@ console.log('Loaded!');
 var element =document.getElementById("elem");
 element.innerHTML="new text";
 var move= document.getElementById("dino");
-var marginleft=0;
-function moveRight(){
+//var marginleft=0;
+/*function moveRight(){
     marginleft=marginleft+'10';
     dino.style.marginleft= marginleft +'px';
-}
+}*/
 dino.onclick = function (){
-    var interval=setInterval(moveRight,50);
+   // var interval=setInterval(moveRight,50);
+    dino.style.marginleft= marginleft + 100;
 };
